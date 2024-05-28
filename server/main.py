a1a78ae5ad46adca8a9df93061796984ac930842
Andy Zijian Zhang <futrime+git@outlook.com>
2024-05-28 15:47:41 +0800

    feat: langchain base

--- server/main.py
+++ server/main.py
@@ -1,5 +1,35 @@
-def main():
-    pass
+"""Main module."""
 
-if __name__ == '__main__':
-    main()
+import asyncio
+import datetime
+
+from langchain_community.llms.tongyi import Tongyi
+from langchain_core.prompts import PromptTemplate
+
+
+async def main():
+    """Main function."""
+
+    llm = Tongyi(model_name="qwen-turbo")  # type: ignore
+
+    template = """Question: {question}
+
+    Answer: Let's think step by step."""
+
+    prompt = PromptTemplate.from_template(template)
+
+    chain = prompt | llm
+
+    question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
+
+    print(datetime.datetime.now())
+
+    ans = await chain.ainvoke({"question": question})
+
+    print(datetime.datetime.now())
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

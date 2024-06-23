b96989bd6a1e09fd0041e20b642d5b8e4aee4e79
Andy Zijian Zhang <futrime+git@outlook.com>
2024-06-23 09:28:50 +0000

    feat(smart_home_server): done

--- smart_home_server/llm.py
+++ smart_home_server/llm.py
@@ -1,44 +1,45 @@
 import json
-from typing import TypedDict
+from typing import List, TypedDict
 
 import dashscope
 import dashscope.api_entities
 
 PROMPT_SYSTEM_TEMPLATE = """
-你是一个先进的访客管理系统助手，负责根据输入的信息综合判断是否允许访客进入。你的决策需综合考虑以下因素：
+你是一个先进的智能管家系统，负责根据输入的信息和事件综合判断是否需要进行某些动作。
 
-1. **人脸识别结果**：匹配表示已注册并授权的访客，不匹配表示未注册或非授权人员，缺失表示无法获取人脸数据。
-2. **指纹识别结果**：匹配表示验证通过，不匹配表示验证未通过，缺失表示未使用指纹识别功能。
-3. **地理位置安全级别**：结合城市和国家的治安情况，评估当前地点的安全风险。
-4. **时间与日期信息**：考虑24小时制的时间段安全性，以及是否为节假日、周末或工作日，以调整安全策略。
-5. **门口照片**：用于环境安全检查，确认无安全隐患。
-6. **访客发言录音**：分析内容，判断是否有威胁性言辞或异常行为指示。
+执行机构描述和命令格式信息：
 
-基于上述信息，你需要输出：
-
-- **准入许可**：明确给出“准入”或“禁止”的决定。如果不能确定，请选择“禁止”。
-- 所有决策必须有一步一步的推理过程，不得出现无法解释的决策。
-- 礼貌而又热情地向访客解释决策的理由。
-
-记住，保护隐私是首要原则，不得要求提供与安全无关的个人信息。你可以访问互联网搜索所需信息。
+```
+{actuators}
+```
 """
 
 PROMPT_USER_TEMPLATE = """
-现在边缘传感系统反馈了以下信息：
+边缘传感系统当前信息：
 
 ```
-{info}
+{sensors}
 ```
 
-你需要首先综合所有信息分析当前状况，并基于谨慎和倾向于禁止的原则进行推理。然后给出准入 / 禁止 的决策，并向访客解释决策的理由。
+事件：
+
+```
+{event}
+```
+
+你需要首先综合所有信息分析当前状况，然后决定是否进行执行器命令。
 你的回复必须绝对严格遵循以下给出的JSON格式。
 JSON回复格式：
 
 ```json
 {{
     "reasoning": "对当前情况的谨慎逐步推理过程，力求详细",
-    "decision": "准入 / 禁止",
-    "explanation": "向访客口头说明的内容，请尽可能热情，不要那么正式。",
+    "actuator_commands": [
+        {{
+            "endpoint": "执行机构的端点",
+            "data": "执行机构的命令，请参照命令格式描述",
+        }}
+    ]
 }}
 ```
 
@@ -46,22 +47,26 @@ JSON回复格式：
 """
 
 
+class ActuatorCommand(TypedDict):
+    endpoint: str
+    data: str
+
+
 class LLMResponse(TypedDict):
     reasoning: str
-    decision: str
-    explanation: str
+    actuator_commands: List[ActuatorCommand]
 
 
 class LLM:
-    def generate(self, info: str) -> LLMResponse:
+    def generate(self, actuators: str, sensors: str, event: str) -> LLMResponse:
         messages = [
             {
                 "role": "system",
-                "content": PROMPT_SYSTEM_TEMPLATE,
+                "content": PROMPT_SYSTEM_TEMPLATE.format(actuators=actuators),
             },
             {
                 "role": "user",
-                "content": PROMPT_USER_TEMPLATE.format(info=info),
+                "content": PROMPT_USER_TEMPLATE.format(sensors=sensors, event=event),
             },
         ]
 
--- smart_home_server/main.py
+++ smart_home_server/main.py
@@ -5,42 +5,24 @@ import json
 import os
 import threading
 import time
-import urllib.parse
 from typing import List
 
-import alibabacloud_facebody20191230.client
 import alibabacloud_facebody20191230.models
-import alibabacloud_tea_openapi.models
-import alibabacloud_tea_util.models
 import holidays
-from comm import ZmqComm
+from communication import ZmqComm
 from info_db import InfoDb, InfoDbEntry
 from llm import LLM
-from schemas import ActuatorCommand, SensorReport
-from tts import TTS
+from schemas import ActuatorCommand, ActuatorRegistration, SensorReport
 from vlm import VLM
 
 COMM_BROKER_HOST = "10.8.0.5"
 COMM_BROKER_BACKEND_PORT = 5556
 COMM_BROKER_FRONTEND_PORT = 5555
 
-QUESTION_LIST: List[str] = [
-    "你住在哪里？",
-    "你怎么来的？",
-    "你的名字是什么？",
-    "你之前来过这里吗？",
-    "你今天来访的目的为何？",
-]
-
-QUESTION_ORDER_PREFIX_LIST: List[str] = [
-    "Hello，欢迎你的到来。在进入之前，还请麻烦您回答几个问题。请问，",
-    "好的，请问，",
-    "最后一个问题了，请问，",
-]
-
 DEFAULT_INFO: List[InfoDbEntry] = []
 
 info_db = InfoDb()
+actuator_db = InfoDb()
 comm = ZmqComm(
     broker_host=COMM_BROKER_HOST,
     broker_backend_port=COMM_BROKER_BACKEND_PORT,
@@ -51,20 +33,31 @@ comm = ZmqComm(
 def main() -> None:
     comm.connect()
     comm.register_receive_callback("sensor", sensor_comm_callback)
+    comm.register_receive_callback("actuator", actuator_registration_callback)
 
     for entry in DEFAULT_INFO:
         info_db.insert(entry)
 
     thread_list: List[threading.Thread] = [
         threading.Thread(target=calendar_thread),
-        threading.Thread(target=face_thread),
         threading.Thread(target=llm_thread),
-        threading.Thread(target=qa_thread),
         threading.Thread(target=vlm_thread),
     ]
     for thread in thread_list:
         thread.start()
 
+def actuator_registration_callback(msg_str: str) -> None:
+    msg = json.loads(msg_str)
+    if msg["messageId"] != "ActuatorRegistration":
+        return
+
+    msg: ActuatorRegistration
+
+    actuator_db.insert({
+        "endpoint": msg["endpoint"],
+        "description": msg["actuatorDescription"],
+        "data": msg["commandFormatDescription"],
+    })
 
 def sensor_comm_callback(msg_str: str) -> None:
     msg = json.loads(msg_str)
@@ -108,87 +101,8 @@ def calendar_thread() -> None:
             }
         )
 
-
-def face_thread() -> None:
-    config = alibabacloud_tea_openapi.models.Config(
-        access_key_id=os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID") or "",
-        access_key_secret=os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET") or "",
-        endpoint="facebody.cn-shanghai.aliyuncs.com",
-        region_id="cn-shanghai",
-    )
-    runtime_options = alibabacloud_tea_util.models.RuntimeOptions()
-
-    last_img_sha256: str = ""
-
-    while True:
-        time.sleep(5)
-
-        if not os.path.exists("img.jpg"):
-            info_db.insert(
-                {
-                    "endpoint": "face",
-                    "description": "本系统用于识别访客的人脸信息，以便进行访客管理",
-                    "data": "缺失",
-                }
-            )
-            continue
-
-        img_hash = hash_with_sha256(open("img.jpg", "rb").read())
-        if img_hash == last_img_sha256:
-            print("No new image")
-            continue
-        last_img_sha256 = img_hash
-
-        stream_a = open("img.jpg", "rb")
-        stream_b = open("face_ref.jpg", "rb")
-        compare_face_request = (
-            alibabacloud_facebody20191230.models.CompareFaceAdvanceRequest(
-                image_urlaobject=stream_a,
-                image_urlbobject=stream_b,
-            )
-        )
-
-        client = alibabacloud_facebody20191230.client.Client(config)
-        try:
-            response = client.compare_face_advance(
-                compare_face_request, runtime_options
-            )
-            confidence = response.body.data.confidence
-            assert isinstance(confidence, float)
-
-            print(response.body.data)
-
-            if confidence > 61:
-                info_db.insert(
-                    {
-                        "endpoint": "face",
-                        "description": "本系统用于识别访客的人脸信息，以便进行访客管理",
-                        "data": "匹配",
-                    }
-                )
-
-            else:
-                info_db.insert(
-                    {
-                        "endpoint": "face",
-                        "description": "本系统用于识别访客的人脸信息，以便进行访客管理",
-                        "data": "不匹配",
-                    }
-                )
-
-        except Exception:
-            info_db.insert(
-                {
-                    "endpoint": "face",
-                    "description": "本系统用于识别访客的人脸信息，以便进行访客管理",
-                    "data": "缺失",
-                }
-            )
-
-
 def llm_thread() -> None:
     llm = LLM()
-    tts = TTS()
 
     last_info_hash: int = 0
 
@@ -202,91 +116,14 @@ def llm_thread() -> None:
             continue
         last_info_hash = info_hash
 
-        llm_response = llm.generate(info)
+        actuator_commands = actuator_db.get()
+
+        llm_response = llm.generate(str(actuator_commands), info, "refresh")
 
         print(llm_response)
 
-        if llm_response["decision"] == "准入":
-            gate_command: ActuatorCommand = {
-                "endpoint": "gate",
-                "messageId": "ActuatorCommand",
-                "data": {
-                    "command": "unlock",
-                },
-            }
-
-        else:
-            gate_command = {
-                "endpoint": "gate",
-                "messageId": "ActuatorCommand",
-                "data": {
-                    "command": "lock",
-                },
-            }
-
-        comm.send("actuator", json.dumps(gate_command))
-
-        if os.path.exists("llm.mp3"):
-            os.remove("llm.mp3")
-
-        tts.generate(llm_response["explanation"], "llm.mp3")
-
-        with open("llm.mp3", "rb") as f:
-            sound_data = f.read()
-
-        sound_command: ActuatorCommand = {
-            "endpoint": "explanation",
-            "messageId": "ActuatorCommand",
-            "data": {
-                "sound": base64.encodebytes(sound_data).decode(),
-            },
-        }
-
-        comm.send("actuator", json.dumps(sound_command))
-
-
-def qa_thread() -> None:
-    tts = TTS()
-
-    while True:
-        time.sleep(5)
-
-        # Generate sound files for each question
-        for question in QUESTION_LIST:
-            for order in range(3):
-                order_prefix = QUESTION_ORDER_PREFIX_LIST[order]
-                text = order_prefix + question
-                file_name = (
-                    hash_with_sha256(urllib.parse.quote(text).encode())[:32] + ".mp3"
-                )
-                if os.path.exists(file_name):
-                    continue
-
-                tts.generate(text, file_name)
-
-        # Send questions to user
-        for question in QUESTION_LIST:
-            for order in range(3):
-                order_prefix = QUESTION_ORDER_PREFIX_LIST[order]
-                text = order_prefix + question
-                file_name = (
-                    hash_with_sha256(urllib.parse.quote(text).encode())[:32] + ".mp3"
-                )
-                with open(file_name, "rb") as f:
-                    sound_data = f.read()
-
-                sound_command: ActuatorCommand = {
-                    "endpoint": "question",
-                    "messageId": "ActuatorCommand",
-                    "data": {
-                        "order": order,
-                        "question": question,
-                        "sound": base64.encodebytes(sound_data).decode(),
-                    },
-                }
-
-                comm.send("actuator", json.dumps(sound_command))
-
+        for actuator_command in llm_response["actuator_commands"]:
+            comm.send("actuator", json.dumps(actuator_command))
 
 def vlm_thread() -> None:
     vlm = VLM()
@@ -300,7 +137,7 @@ def vlm_thread() -> None:
             info_db.insert(
                 {
                     "endpoint": "vlm",
-                    "description": "本系统用于分析图片中的场景，以便进行安防推理",
+                    "description": "本系统用于分析图片中的场景，以便进行环境分析",
                     "data": "缺失",
                 }
             )
@@ -319,7 +156,7 @@ def vlm_thread() -> None:
         info_db.insert(
             {
                 "endpoint": "vlm",
-                "description": "本系统用于分析图片中的场景，以便进行安防推理",
+                "description": "本系统用于分析图片中的场景，以便进行环境分析",
                 "data": vlm_response,
             }
         )
